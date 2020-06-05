65a87b5222c1049f53a34f48fd1b7fbef7f322a2
NGD <jch5201@naver.com>
2020-06-06 06:22:01 +0900

    add movnti

--- llvm/lib/Target/X86/UnalignedGadget.cpp
+++ llvm/lib/Target/X86/UnalignedGadget.cpp
@@ -45,6 +45,8 @@ namespace {
     bool isVulnerableJmp(MachineInstr &MI);
     bool isVulnerableBswap(MachineInstr &MI);
     bool changeVulnerableBswap(MachineInstr &MI);
+    bool isVulnerableMovnti(MachineInstr &MI);
+    bool changeVulnerableMovnti(MachineInstr &MI);
     // Add fields and helper functions for this pass here.
   };
 }
@@ -61,6 +63,7 @@ char UnalignedGadgetRemoval::ID = 0;
 
 
 bool UnalignedGadgetRemoval::runOnMachineFunction(MachineFunction &MF) {
+  bool changed = false;
 
   MachineModuleInfo &MMI =
       getAnalysis<MachineModuleInfoWrapperPass>().getMMI();
@@ -70,12 +73,18 @@ bool UnalignedGadgetRemoval::runOnMachineFunction(MachineFunction &MF) {
         errs() << "Found vulnerable jmp op\n" ;
       }
       else if (isVulnerableBswap(MI)) {
+        changeVulnerableBswap(MI);
+        changed = true;
         errs() << "Found vulnerable bswap op\n" ;
+      }
+      else if (isVulnerableMovnti(MI)) {
+        changeVulnerableMovnti(MI);
+        changed = true;
+        errs() << "Found vulnerable movnti op\n" ;
       }
     }
   }
-  return true;
-
+  return changed;
 }
 
 bool UnalignedGadgetRemoval::isVulnerableJmp(MachineInstr &MI) {
@@ -133,6 +142,33 @@ bool UnalignedGadgetRemoval::changeVulnerableBswap(MachineInstr &MI) {
   MIB = BuildMI(*MBB, &MI, DL, TII.get(MI.getOpcode())).addReg(newReg, RegState::Define).addReg(newReg, RegState::Kill);
   MIB = BuildMI(*MBB, &MI, DL, TII.get(X86::MOV32rr)).addReg(oldReg).addReg(newReg);
   MIB = BuildMI(*MBB, &MI, DL, TII.get(X86::POP64r)).addReg(X86::RCX, RegState::Define);
+
+  MI.removeFromParent();
+}
+
+bool UnalignedGadgetRemoval::isVulnerableMovnti(MachineInstr &MI) {
+  if (MI.getOpcode() == X86::MOVNTImr || MI.getOpcode() == X86::MOVNTI_64mr)
+    return true;
+  else
+    return false;
+}
+
+bool UnalignedGadgetRemoval::changeVulnerableMovnti(MachineInstr &MI) {
+  MachineBasicBlock *MBB =  MI.getParent();
+  MachineFunction *MF = MBB->getParent();
+  const X86Subtarget &STI = MF->getSubtarget<X86Subtarget>();
+  const X86InstrInfo &TII = *STI.getInstrInfo();
+  DebugLoc DL = MI.getDebugLoc();
+  MachineInstrBuilder MIB; 
+
+  bool is32 = MI.getOpcode() == X86::MOVNTImr;
+  unsigned int moveOp = is32 ? X86::MOV32mr : X86::MOV64mr;
+
+  MIB = BuildMI(*MBB, &MI, DL, TII.get(moveOp));
+  MachineOperand *MO = new MachineOperand(MI.getOperand(0));
+  MIB.addOperand(*MO);
+  *MO = new MachineOperand(MI.getOperand(1));
+  MIB.addOperand(*MO);
 
   MI.removeFromParent();
 }
